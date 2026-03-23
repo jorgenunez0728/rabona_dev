dd66ae5ff8b355cd339528f497b882b23eec1146
Jorge Alberto Nuñez de León <jorgenunez0728@gmail.com>
2026-03-23 04:50:31 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ const AuthenticatedApp = () => {
   return (
     <Routes>
       {/* Add your page Route elements here */}
+      <Route path="/" element={<Rabona />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
